259b8b63a5c272440d3caac8aaf324b52d910c3a
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-07 15:22:00 +0530

    First Commit

---
+++ src/component/SideBar/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SideBar.css';
+import {assets} from '../../assets/assets';
+
+const SideBar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">
+            <img src={assets.menu_icon} alt=''></img>
+        </div>
+        <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default SideBar
