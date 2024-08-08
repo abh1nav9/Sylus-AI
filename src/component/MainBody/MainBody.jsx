3350ea6387c08308ecc121c4ab22a59dbb1cab3f
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-08 16:49:43 +0530

    Loader and Typing Animation Added

--- src/component/MainBody/MainBody.jsx
+++ src/component/MainBody/MainBody.jsx
@@ -27,78 +27,84 @@ const MainBody = () => {
         <img src={assets.user_icon} alt="User Icon" />
       </div>
       <div className="mainContainer">
-        <div>
-          {!showResult ? (
-            <>
-              <div className="greet">
-                <div>
-                  <div className="span">Hello There</div>
-                </div>
-                <div>How can I help you today?</div>
-              </div>
-              <div className="cards">
-                <div className="card">
-                  <div className="cardParagraph">
-                    Suggest beautiful places to see on an upcoming road trip
-                  </div>
-                  <img src={assets.compass_icon} alt="Compass Icon" />
-                </div>
-                <div className="card">
-                  <div className="cardParagraph">
-                    Briefly summarize this concept: urban planning
-                  </div>
-                  <img src={assets.bulb_icon} alt="Bulb Icon" />
-                </div>
-                <div className="card">
-                  <div className="cardParagraph">
-                    Brainstorm team bonding activities for our work retreat
-                  </div>
-                  <img src={assets.message_icon} alt="Message Icon" />
-                </div>
-                <div className="card">
-                  <div className="cardParagraph">
-                    Tell me about ReactJS and React Native
-                  </div>
-                  <img src={assets.code_icon} alt="Code Icon" />
-                </div>
-              </div>
-            </>
-          ) : (
-            <div className="result">
-              <div className="resultTitle">
-                <img src={assets.user_icon} alt="User Icon" />
-                <div>{recentPrompt}</div>
-              </div>
-              <div className="resultData">
-                <img src={assets.gemini_icon} alt="Sylus AI" />
-                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
-              </div>
-            </div>
-          )}
-        </div>
-        <div>
-          <div className="mainBottom">
-            <div className="searchBox">
-              <input
-                onChange={(e) => setInput(e.target.value)}
-                value={input}
-                type="text"
-                placeholder="Enter a promt here ..."
-              />
+        {!showResult ? (
+          <>
+            <div className="greet">
               <div>
-                <img src={assets.gallery_icon} alt="Gallery Icon" />
-                <img src={assets.mic_icon} alt="Mic Icon" />
-                <img
-                  onClick={() => onSent()}
-                  src={assets.send_icon}
-                  alt="Send Icon"
-                />
+                <div className="span">Hello There</div>
+              </div>
+              <div>How can I help you today?</div>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <div className="cardParagraph">
+                  Suggest beautiful places to see on an upcoming road trip
+                </div>
+                <img src={assets.compass_icon} alt="Compass Icon" />
+              </div>
+              <div className="card">
+                <div className="cardParagraph">
+                  Briefly summarize this concept: urban planning
+                </div>
+                <img src={assets.bulb_icon} alt="Bulb Icon" />
+              </div>
+              <div className="card">
+                <div className="cardParagraph">
+                  Brainstorm team bonding activities for our work retreat
+                </div>
+                <img src={assets.message_icon} alt="Message Icon" />
+              </div>
+              <div className="card">
+                <div className="cardParagraph">
+                  Tell me about ReactJS and React Native
+                </div>
+                <img src={assets.code_icon} alt="Code Icon" />
               </div>
             </div>
-            <div className="bottomInfo">
-              Sylus AI may display inaccurate info, including about people, so
-              double-check its responses.
+          </>
+        ) : (
+          <div className="result">
+            <div className="resultTitle">
+              <img src={assets.user_icon} alt="User Icon" />
+              <div>{recentPrompt}</div>
             </div>
+            <div className="resultData">
+              <img src={assets.gemini_icon} alt="Sylus AI" />
+              {loading 
+              ? 
+              <div className="loader">
+                <hr />
+                <hr />
+                <hr />
+              </div>
+              :
+              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              }
+            </div>
+          </div>
+        )}
+
+        <div className="mainBottom">
+          <div className="searchBox">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a promt here ..."
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="Gallery Icon" />
+              <img src={assets.mic_icon} alt="Mic Icon" />
+              <img
+                onClick={() => onSent()}
+                src={assets.send_icon}
+                alt="Send Icon"
+              />
+            </div>
+          </div>
+          <div className="bottomInfo">
+            Sylus AI may display inaccurate info, including about people, so
+            double-check its responses.
           </div>
         </div>
       </div>
